17a36079196ad764c0ff6bac24911602f6588814
unknown <singhdeveloper937@gmail.com>
2023-10-02 19:56:46 +0530

    input function commit.

---
+++ Input.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 19:46:34 2023
+
+@author: Abhi
+"""
+#input function to take the input from the user.
+#It waits for the user to respond to the prompt.
+#Basic Sysntax.
+name= input("What is your name")
+print("Hello \n" +name)
+#This programe can be done in following way as well.
+print("Hello" + input ("What is your name"))
