95766b6f2dba053df4739fc0cd907019352189d0
unknown <singhdeveloper937@gmail.com>
2023-10-02 19:16:34 +0530

    Hello World in python commit.

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 18:58:52 2023
+
+@author: Abhi
+"""
+#This is the first program the default hello world programme
+#Print function prints your thoughts on the console
+print("Hello"+" "+"World")
